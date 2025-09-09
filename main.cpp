9c403885438aa5d4e7729e0a61a6b6cc801ebac1
ZayedKh <zayedkh02@gmail.com>
2025-09-09 12:07:36 +0100

    Added types

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+using Price = int32_t;
+using Quantity = uint32_t;
+using OrderId = std::string;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
