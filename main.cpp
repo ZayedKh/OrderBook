7d20fd4183eedd9cb90d14e0e1aadac8047bcbb2
ZayedKh <zayedkh02@gmail.com>
2025-09-14 13:00:43 +0100

    Serious work only now

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
-using Price = int32_t;
-using Quantity = uint32_t;
-using OrderId = std::string;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
